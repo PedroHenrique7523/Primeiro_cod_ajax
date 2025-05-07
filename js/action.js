e80a0f7813b2d91019be19e4e04b5620054a6c80
PedroHenrique7523 <140754899+PedroHenrique7523@users.noreply.github.com>
2025-05-07 11:59:30 -0300

    Add files via upload

---
+++ js/action.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const button = document.getElementById('carregar');
+
+    button.addEventListener('click', function () {
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', './php/exemplophp.php', true);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                alert('Dados recebidos: ' + xhr.responseText);
+            } else {
+                alert('Erro na requisição: ' + xhr.status);
+            }
+        };
+        xhr.onerror = function () {
+            alert('Ocorreu um erro na requisição');
+        };
+
+        xhr.send();
+    });
+
+});
